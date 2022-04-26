e319146904f74e975e24f033fca0c085a762cd59
szeu <szewingkwan2000@gmail.com>
2022-04-26 18:25:28 +0800

    lab 9 finished

---
+++ src/lab9/FindMax.java
@@ -0,0 +1,100 @@
+package lab9;
+
+/**
+ * 
+ * COMP 3021
+ * 
+This is a class that prints the maximum value of a given array of 90 elements
+
+This is a single threaded version.
+
+Create a multi-thread version with 3 threads:
+
+one thread finds the max among the cells [0,29] 
+another thread the max among the cells [30,59] 
+another thread the max among the cells [60,89]
+
+Compare the results of the three threads and print at console the max value.
+
+ * 
+ * @author valerio
+ *
+ */
+public class FindMax {
+	// this is an array of 90 elements
+	// the max value of this array is 9999
+	static int[] array = { 1, 34, 5, 6, 343, 5, 63, 5, 34, 2, 78, 2, 3, 4, 5, 234, 678, 543, 45, 67, 43, 2, 3, 4543,
+			234, 3, 454, 1, 2, 3, 1, 9999, 34, 5, 6, 343, 5, 63, 5, 34, 2, 78, 2, 3, 4, 5, 234, 678, 543, 45, 67, 43, 2,
+			3, 4543, 234, 3, 454, 1, 2, 3, 1, 34, 5, 6, 5, 63, 5, 34, 2, 78, 2, 3, 4, 5, 234, 678, 543, 45, 67, 43, 2,
+			3, 4543, 234, 3, 454, 1, 2, 3 };
+
+	public static void main(String[] args) {
+		new FindMax().printMax();
+	}
+	
+	class MaxThread implements Runnable {
+		private int begin;
+		private int end;
+		private int max;
+		
+		public MaxThread(int begin, int end) {
+			this.begin = begin;
+			this.end = end;
+		}
+		
+		@Override
+		public void run() {
+			this.max = findMax(begin, end);
+		}
+		
+		public int getMax() {
+			return max;
+		}
+	}
+
+	public void printMax() {
+		// this is a single threaded version
+		
+		MaxThread part1 = new MaxThread(0, 29);
+		MaxThread part2 = new MaxThread(30, 59);
+		MaxThread part3 = new MaxThread(60, 89);
+		
+		Thread t1 = new Thread(part1);
+		Thread t2 = new Thread(part2);
+		Thread t3 = new Thread(part3);
+		
+		t1.start();
+		t2.start();
+		t3.start();
+		
+		try {
+			t1.join();
+			t2.join();
+			t3.join();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		int max = Math.max(Math.max(part1.getMax(), part2.getMax()), part3.getMax());
+		System.out.println("the max value is " + max);
+	}
+
+	/**
+	 * returns the max value in the array within a give range [begin,range]
+	 * 
+	 * @param begin
+	 * @param end
+	 * @return
+	 */
+	private int findMax(int begin, int end) {
+		// you should NOT change this function
+		int max = array[begin];
+		for (int i = begin + 1; i <= end; i++) {
+			if (array[i] > max) {
+				max = array[i];
+			}
+		}
+		return max;
+	}
+}
